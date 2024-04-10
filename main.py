4dc0dd4ccf997951a596c0156b4ddc5263cb7ef2
Daniel Morawski <gvrmvnn@gmail.com>
2024-04-10 12:05:27 +0200

    Add comments main.py

--- main.py
+++ main.py
@@ -11,12 +11,15 @@ from FixedFileIO.utils import ValidationExecutor
 logger = logging.getLogger(__name__)
 
 
-def read_values(handler: FixedWidthHandler) -> None:
+def read_values_cli(handler: FixedWidthHandler) -> None:
+    """CLI function for display file's values"""
     header, transactions, footer = handler.read_file()
     print(header)
 
+    # Initialize set of currencies
     currencies = set()
 
+    # Popping 'Reserved' field
     for transaction in transactions:
         transaction.pop('Reserved', None)
         currencies.add(transaction['Currency'])
@@ -24,24 +27,33 @@ def read_values(handler: FixedWidthHandler) -> None:
     footer.pop('Reserved', None)
     print(footer)
 
+    # Display currency warning
     if len(currencies) > 1:
-        print(f"Control sum is not representative due to different currencies: {currencies}")
+        message = f"Control sum is not representative due to different currencies: {currencies}"
+        logger.warning(message)
+        print(message)
 
 
 def add_transaction_cli(handler: FixedWidthHandler) -> None:
+    """CLI function for managing add transactions"""
     amount = input("Enter the transaction amount: ")
+
+    # Check whether amount can be float type
     try:
         amount = "{:.2f}".format(float(amount))
     except ValueError:
         print("Invalid amount. Please enter a numeric value.")
         return
 
-    currency = input("Enter the currency code: ").upper()
+    currency = input("Enter the currency code: ").upper()  # Make currency insertion flexible
 
     handler.add_transaction(amount=amount, currency=currency)
 
 
 def update_field_cli(handler: FixedWidthHandler) -> None:
+    """CLI function for managing update file fields"""
+
+    # Load permissions for updating fields
     field_permissions = _load_field_permissions()
 
     record_type = input("Enter record to update: ")
@@ -51,6 +63,7 @@ def update_field_cli(handler: FixedWidthHandler) -> None:
         raise KeyError(f"Updating field '{field_name}' is not allowed.")
 
     field_value = input("Enter the new value: ")
+    # Initialize counter as None for no transaction fields
     counter = None
     if record_type == 'transaction':
         counter = input("Enter the transaction counter: ").zfill(6)
@@ -60,12 +73,14 @@ def update_field_cli(handler: FixedWidthHandler) -> None:
                          counter=counter)
 
 
-def _load_field_permissions(filepath='configs/permissions.json') -> dict:
+def _load_field_permissions(filepath: str = 'configs/permissions.json') -> dict:
+    """Auxiliary function for load permissions"""
     with open(filepath, 'r') as file:
         return json.load(file)
 
 
-def _save_field_permissions(settings, filepath='configs/permissions.json') -> None:
+def _save_field_permissions(settings: dict, filepath: str = 'configs/permissions.json') -> None:
+    """Auxiliary function for save permissions"""
     try:
         with open(filepath, 'w') as file:
             json.dump(settings, file, indent=4)
@@ -73,9 +88,12 @@ def _save_field_permissions(settings, filepath='configs/permissions.json') -> No
         print(f"Saving configs failed due to: {e}")
 
 
-def change_permissions() -> None:
+def change_permissions_cli() -> None:
+    """CLI function for managing file fields permissions"""
     permissions = _load_field_permissions()
     print("Current field permissions:")
+
+    # Display current permissions
     for field, permission in permissions.items():
         print(f"{field}: {'Blocked' if permission else 'Allowed'}")
     field_to_change = input("Enter field name to change permission: ")
@@ -108,30 +126,34 @@ def setup_logger() -> None:
 
 
 def main() -> None:
+    # Parser configs
     parser = argparse.ArgumentParser(description='CLI for Fixed File IO operations.')
     parser.add_argument('action', choices=['read', 'add', 'update', 'settings'], help='Action to perform.')
     parser.add_argument('filepath', help='Path to the fixed-width file.')
 
+    # Initializations
     args = parser.parse_args()
     setup_logger()
     handler = FixedWidthHandler(filepath=args.filepath)
     validation = ValidationExecutor(filepath=args.filepath)
 
+    # Validation check
     status, results = validation.run()
     if status:
         print(f"Validation success: {results}")
     else:
         print(f"Validation failure: {results}")
 
+    # Main
     match args.action:
         case 'read':
-            read_values(handler)
+            read_values_cli(handler)
         case 'add':
             add_transaction_cli(handler)
         case 'update':
             update_field_cli(handler)
         case 'settings':
-            change_permissions()
+            change_permissions_cli()
 
 
 if __name__ == '__main__':
