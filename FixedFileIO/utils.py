83d7e22cf09110ec5cab407d5f72de5682c7dda9
Daniel Morawski <gvrmvnn@gmail.com>
2024-04-10 08:25:51 +0200

    Add logging and short documentation in utils.py

--- FixedFileIO/utils.py
+++ FixedFileIO/utils.py
@@ -17,110 +17,151 @@ class TransactionLimitError(Exception):
 
 
 def get_values_as_dict(line, slices) -> dict:
+    """Extracts substrings from a line based on the provided
+     slices and returns them as a dictionary."""
     values = {}
     for key, (start, end) in slices.items():
         values[key] = line[start:end].strip()
     return values
 
 
+def success(log_message) -> bool:
+    """Logs a success message and returns True."""
+    logger.info(log_message)
+    return True
+
+
+def failure(log_message) -> bool:
+    """Logs a success message and returns False."""
+    logger.error(log_message)
+    return False
+
+
 class Validation:
+    """Class used for validation proper file format"""
     def __init__(self, filepath):
         self.filepath = filepath
 
     @staticmethod
-    def _validate_header(line) -> (bool, str):
+    def _validate_line_length(line) -> bool:
+        """Checks if line length is equal 120"""
+        return False if len(line.rstrip('\n')) != 120 else True
+
+    def validate_header(self, line) -> bool:
+        """Validation of file's header"""
         slices = const.HEADER_SLICES
 
+        if not self._validate_line_length(line):
+            failure(log_message=f"Required line length: 120 Actual: {len(line) - 1}")  # - \n
+
+        # Get values of fields using constant slices
         field_id = line[slices['Field ID'][0]:slices['Field ID'][1]]
         name = line[slices['Name'][0]:slices['Name'][1]].strip()
         surname = line[slices['Surname'][0]:slices['Surname'][1]].strip()
         patronymic = line[slices['Patronymic'][0]:slices['Patronymic'][1]].strip()
         address = line[slices['Address'][0]:slices['Address'][1]].strip()
+        logger.debug(f"Values: {field_id}, {name}, {surname}, {patronymic}, {address}")
 
         # Field ID validation
         if not re.match(r'^01$', field_id):
-            return False, "Invalid Field ID in header."
+            return failure(log_message="Invalid Field ID in header.")
 
         # Name, Surname, Patronymic validation
-        for item in [name, surname, patronymic]:
+        for key, item in {"Name": name, "Surname": surname, "Patronymic": patronymic}:
             if not re.match(r'^[A-Za-z\s]+$', item):
-                return False, f"{item} is invalid"
+                return failure(log_message=f"Invalid {key} in header")
 
         # Address validation
         if not re.match(r'^[\w\s,.]+$', address):
-            return False, "Invalid Address in header."
+            return failure(log_message="Invalid Address in header.")
 
-        return True, "Header is valid."
+        return success(log_message="Header is valid.")
 
-    @staticmethod
-    def _validate_transactions(lines) -> (bool, str):
+    def validate_transactions(self, lines) -> bool:
+        """Validation of file's transactions"""
         slices = const.TRANSACTIONS_SLICES
 
+        # Check every line
         for line in lines:
+            if not self._validate_line_length(line):
+                failure(log_message=f"Required line length: 120 Actual: {len(line) - 1}")  # - \n
+
+            # Get values of fields using constant slices
             field_id = line[slices['Field ID'][0]:slices['Field ID'][1]]
             counter = line[slices['Counter'][0]:slices['Counter'][1]]
             amount = line[slices['Amount'][0]:slices['Amount'][1]]
             currency = line[slices['Currency'][0]:slices['Currency'][1]]
 
+            logger.debug(f"Values: {field_id}, {counter}, {amount}, {currency}")
+
             # Field ID validation
             if not re.match(r'^02$', field_id):
-                return False, "Invalid Field ID in transaction"
+                return failure(log_message="Invalid Field ID in transaction.")
 
             # Counter validation
             if not re.match(r'^\d{6}$', counter):
-                return False, "Counter format is not correct."
+                return failure(log_message="Counter format is not correct.")
 
             # Amount validation
             if not re.match(r'^\d{12}$', amount):
-                return False, "Amount format is not correct."
+                return failure(log_message="Amount format is not correct.")
 
             # Currency validation
             if currency not in const.CURRENCIES:
-                return False, f"Invalid currency '{currency}' in transaction."
+                return failure(log_message=f"Invalid currency '{currency}' in transaction.")
 
-        return True, "All transactions are valid"
+        return success(log_message="All transactions are valid.")
 
-    @staticmethod
-    def _validate_footer(line, num_transactions, total_amount) -> (bool, str):
+    def validate_footer(self, line, num_transactions, total_amount) -> bool:
+        """Validation of file's footer"""
         slices = const.FOOTER_SLICES
 
+        if not self._validate_line_length(line):
+            failure(log_message=f"Required line length: 120 Actual: {len(line) - 1}")  # - \n
+
+        # Get values of fields using constant slices
         field_id = line[slices['Field ID'][0]:slices['Field ID'][1]]
         total_counter = line[slices['Total Counter'][0]:slices['Total Counter'][1]]
         control_sum = line[slices['Control sum'][0]:slices['Control sum'][1]]
 
         # Field ID validation
         if not re.match(r'^03$', field_id):
-            return False, "Invalid Field ID in footer"
+            return failure(log_message="Invalid Field ID in footer.")
 
         # Total Counter validation
         if not re.match(r'^\d{6}$', total_counter):
-            return False, "Total Counter format is not correct."
+            return failure(log_message="Total Counter format is not correct.")
         if int(total_counter) != num_transactions:
-            return False, f"Total Counter value does not match the number of transactions ({num_transactions})."
+            return failure(log_message=f"Total Counter does not match the number of transactions ({num_transactions}).")
 
         # Control Sum validation
         if not re.match(r'^\d{12}$', control_sum):
-            return False, "Control Sum format is not correct."
+            return failure(log_message="Control Sum format is not correct.")
         if int(control_sum) != total_amount:
-            return False, f"Control Sum value does not match the total amount of transactions ({total_amount})."
+            return failure(log_message=f"Control Sum does not match the total amount of transactions ({total_amount}).")
 
-        return True, "Footer is valid"
+        return success(log_message="Footer is valid.")
 
     def run(self) -> (bool, list):
+        """Validation of whole file"""
         with open(self.filepath, 'r', encoding='utf-8') as file:
             lines = file.readlines()
 
-            num_transactions = len(lines) - 2
+            num_transactions = len(lines) - 2  # - (header+footer)
+
+            # Indices of Amount field
             amount_start = const.TRANSACTIONS_SLICES['Amount'][0]
             amount_end = const.TRANSACTIONS_SLICES['Amount'][1]
+            # Sum of transactions Amounts
             total_amount = sum(int(line[amount_start:amount_end]) for line in lines if line.startswith('02'))
 
-            results = [self._validate_header(line=lines[0]),
-                       self._validate_transactions(lines=lines[1:-1]),
-                       self._validate_footer(line=lines[-1],
-                                             num_transactions=num_transactions,
-                                             total_amount=total_amount)]
+            # List of results of particular validations
+            results = [self.validate_header(line=lines[0]),
+                       self.validate_transactions(lines=lines[1:-1]),
+                       self.validate_footer(line=lines[-1],
+                                            num_transactions=num_transactions,
+                                            total_amount=total_amount)]
 
-            if all(results[0]):
-                return True, results
-            return False, results
+            if all(results):
+                return success(log_message=f"Validation OK. Results: {results}"), results
+            return failure(log_message=f"Validation NOK. Results: {results}"), results
